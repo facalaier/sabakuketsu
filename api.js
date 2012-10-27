af0d43353be63c0847faaf44390ef2c22b325465
Marcos Costa <marcos.webdevelop@yahoo.com.br>
2012-10-27 21:25:33 -0200

    Criei agora!

---
+++ api.js
@@ -0,0 +1,183 @@
+﻿jQuery(function() {
+				jQuery.getJSON("http://brasilia.deboa.com/mobileinterface.php?f=templateFilme&jsoncallback=?", function(templates) {
+					jQuery.each(templates, function (i, template) {
+						ich.addTemplate(template.nome, template.string);
+					});
+				});
+			});
+			jQuery(function() {
+				jQuery.getJSON("http://brasilia.deboa.com/mobileinterface.php?f=datasDisponiveis&jsoncallback=?", function(data) {
+					jQuery("#data").append('<option value="">Selecione uma data</option>');
+					jQuery.each(data,function(i,data){
+						dataformat = data.split("-");
+						jQuery("#data").append('<option value="'+data+'">'+dataformat[2]+'/'+dataformat[1]+'/'+dataformat[0]+'</option>');
+					});
+					jQuery("label[for='data']").hide();
+				});
+			});
+			jQuery(function() {
+				jQuery.getJSON("http://brasilia.deboa.com/mobileinterface.php?f=filmesDisponiveis&jsoncallback=?",	function(filme) {
+					jQuery("#filme").append('<option value="">Todos os filmes</option>');
+					jQuery.each(filme,function(i,filme){
+						jQuery("#filme").append(
+							'<option value="'+filme.id_filme+'">'+filme.nome+'</option>'
+						);
+					});
+					jQuery("label[for='filme']").hide();
+				});
+			});
+			jQuery(function() {
+				jQuery.getJSON("http://brasilia.deboa.com/mobileinterface.php?f=locaisDisponiveis&jsoncallback=?",	function(local) {
+					jQuery("#local").append('<option value="">Todos os cinemas</option>');
+					jQuery.each(local,function(i,local){
+						jQuery("#local").append(
+							'<option value="'+local.id_localidade+'">'+local.nome+'</option>'
+						);
+					});
+					jQuery("label[for='local']").hide();
+				});
+			});
+			
+			function limpa(){
+				jQuery("#ichfilme, #listafilmes, #nome, #alert").html("");
+			}
+			
+			function imprimeHorariosCinemas(id, data){
+				var nome = '';
+				var vazio = true;
+				var div = '#'+id+' .h';
+				for(horarios in data){
+					var horario = data[horarios];
+					var hora = horario.horario.split("");
+					if(horario.nome != nome){
+					jQuery(div).append("<br/><a href='#' class='cinema' id='"+horario.id_localidade+"'>&raquo;"+horario.nome+" </a>");
+					jQuery(div).append(hora[0]+hora[1]+":"+hora[2]+hora[3]);
+					vazio = false;
+					} else {
+					jQuery(div).append(" - "+hora[0]+hora[1]+":"+hora[2]+hora[3]);
+					}				
+					nome = horario.nome;
+				}
+				if (vazio){
+					jQuery("p.horarios").hide();
+					jQuery(div).append("<b>Esse filme não está disponível nessa data. Tente buscar por outra data.</b>");
+				}
+			}
+			
+			function imprimeHorariosSimples(id, data){
+				var nome = '';
+				var div = '#'+id+' .h';
+				for(horarios in data){
+					var horario = data[horarios];
+					var hora = horario.horario.split("");
+					if(horario.nome != nome){
+					jQuery(div).append("<br/>");
+					jQuery(div).append(hora[0]+hora[1]+":"+hora[2]+hora[3]);
+					} else {
+					jQuery(div).append(" - "+hora[0]+hora[1]+":"+hora[2]+hora[3]);
+					}
+					nome = horario.nome;
+				}
+			}
+			
+			function pesquisaFilme(filme, datas){
+				limpa();
+				jQuery('#load').show();
+				jQuery.getJSON("http://brasilia.deboa.com/mobileinterface.php?f=pesquisaFilme&filme="+filme+"&data="+datas+"&jsoncallback=?", function(data) {
+					var filme = ich.completo(data[1][0]);
+					jQuery('#ichfilme').append(filme);
+					var filmeHoras = data[0];
+					var hnome = '';
+					var id = data[1][0][0];
+					imprimeHorariosCinemas(id, filmeHoras);
+					jQuery('#load').hide();
+					jQuery('input[type=submit]').removeAttr('disabled');
+				});
+			}
+		
+			function pesquisaFilmeLocal(filme, local, datas){
+				limpa();
+				jQuery('#load').show();
+				jQuery.getJSON("http://brasilia.deboa.com/mobileinterface.php?f=pesquisaFilmeLocal&filme="+filme+"&local="+local+"&data="+datas+"&jsoncallback=?", function(data) {
+					var cinema = jQuery('#local option[value='+local+']').text();
+					jQuery('#ichfilme').append('<h4>'+cinema+'</h4>');
+					var filmeData = data[1][0];
+					var id = data[1][0][0];
+					var filmeHoras = data[0];
+					if(filmeHoras == ''){
+						jQuery('#alert').append("<p>"+filmeData.nome+" não está em cartaz nesse cinema</p>");
+						jQuery('#alert').append("<a class='cinema' id='"+local+"' href='#'>Veja os outros filmes desse cinema</a> ou <a class='filme' id='"+id+"' href='#'>Veja aonde está passando esse filme</a>");
+					} else {
+					var filme = ich.completo(filmeData);
+					jQuery('#ichfilme').append(filme);
+					imprimeHorariosCinemas(id, filmeHoras);
+					}
+					jQuery('#load').hide();
+					jQuery('input[type=submit]').removeAttr('disabled');
+				});
+			}
+			
+			function pesquisaLocal(local, datas){
+				limpa();
+				jQuery('#load').show();
+				jQuery.getJSON("http://brasilia.deboa.com/mobileinterface.php?f=pesquisaLocal&local="+local+"&data="+datas+"&jsoncallback=?",	function(data) {
+					var cinema = jQuery('#local option[value='+local+']').text();
+					jQuery('#ichfilme').append('<h4>'+cinema+'</h4>');
+					for(filmes in data){
+						var filmeData = data[filmes];
+						var filme = ich.filme(filmeData);
+						jQuery('#ichfilme').append(filme);
+						imprimeHorariosSimples(filmeData.id_filme, filmeData.horario);
+					}
+					jQuery('#load').hide();
+					jQuery('input[type=submit]').removeAttr('disabled');
+				});
+			}
+			
+			function pesquisa(datas){
+				limpa();
+				jQuery('#load').show();
+				jQuery.getJSON("http://brasilia.deboa.com/mobileinterface.php?f=pesquisa&data="+datas+"&jsoncallback=?",	function(data) {
+					for(filmes in data){
+						var filmeData = data[filmes];
+						var filme = ich.filme(filmeData);
+						jQuery('#ichfilme').append(filme);
+						imprimeHorariosCinemas(filmeData.id_filme, filmeData.horario);
+						jQuery('#load').hide();
+					}
+					jQuery('input[type=submit]').removeAttr('disabled');
+				});
+			}
+			
+			
+			function controle(filme, local, data){
+				
+				// Se a data estiver vazia abre alerta e volta.
+				
+				if(data==''){
+					
+					
+					
+					alert('Escolha uma data!');			
+					window.history.back();
+					
+
+
+				} else{
+					jQuery('input[type=submit]').attr('disabled', 'disabled');
+					if(filme==''){
+						if(local==''){
+							pesquisa(data);
+						} else {
+							pesquisaLocal(local, data);
+						}
+					} else {
+						if(local==''){
+							pesquisaFilme(filme, data);
+						} else {
+							pesquisaFilmeLocal(filme, local, data);
+						}
+					}
+				}
+			}
+			
---
+++ mobileinterface.php
@@ -0,0 +1,227 @@
+﻿ <?php
+	// banco
+
+	$host = "localhost";
+	$db = "cinema";
+	$user = "cinema";
+	$pass = "VLZ1604vlz";
+	mysql_connect($host, $user, $pass) or die(mysql_error());
+	mysql_select_db($db) or die(mysql_error()); 
+	
+	// chamando os metodos
+
+
+	if(isset($_GET['f']) && !empty($_GET['f'])) {
+		if(function_exists($_GET['f'])) {
+			$_GET['f']();
+		}
+	}
+
+
+	
+	/*
+	
+	$template['string'] = '<div id="{{id_filme}}"><img src="http://brasilia.deboa.com/imagens/cinema/{{id_filme}}.jpg"/><h2><a href="#" id="{{id_filme}}" class="filme">{{nome}}</a></h2><p>Gênero: {{genero}}</p><p>Classificação: {{classificacao}} anos</p><a href="#" id="{{id_filme}}" class="filme">Ver sinopse e trailer</a><br/><p class="horarios">Horário(s): </p><div class="h"></div></div>';
+	
+	$template['string'] = '<div id="{{id_filme}}"><img src="http://brasilia.deboa.com/imagens/cinema/{{id_filme}}.jpg"/><h2><a href="#" id="{{id_filme}}" class="filme">{{nome}}</a></h2><p>Gênero: {{genero}}</p><p>Classificação: {{classificacao}} anos</p><p>{{descricao}}</p><br/><p class="horarios">Horário(s): </p><div class="h"></div><p>Trailer: </p>{{trailer}}</div>';
+	
+	*/
+	
+	
+	function templateFilme(){
+		$template = array();
+		$result = array();
+		$template['nome'] = 'filme';
+		$template['string'] = '<div id="{{id_filme}}">
+<table width="320px" border="0" cellspacing="0" cellpadding="0" bgcolor="#E6E6E6" background="/imagens/back_nome_cinema.png">   <tr> <td colspan="2" align="center" background="/imagens/back_nome_cinema.png"></td> </tr>    <tr> <td rowspan="2"><img src="http://brasilia.deboa.com/imagens/cinema/{{id_filme}}.jpg" width="135px" height="110px"/></td> <td>  <a href="#" id="{{id_filme}}" class="filme"><font color="#333333" size="3"><b>{{nome}}</b></font></a><br /> <font color="#333333" size="2"> <b>Gênero:</b> {{genero}}<br /> <b>Classificação:</b> {{classificacao}} anos  </font> </td> </tr> <tr> <td><a href="#" id="{{id_filme}}" class="filme"><b>Ver sinopse</b></a></td> </tr> <tr> <td colspan="2"> <p class="horarios"><font color="#333333" size="3"><b>Horário(s):</b></font></p><div class="h"></div> </td> </tr> </table>
+</div>';
+		$result[] = $template;
+		$template['nome'] = 'completo';
+		$template['string'] = '<div id="{{id_filme}}"><img src="http://brasilia.deboa.com/imagens/cinema/{{id_filme}}.jpg"/><h2><a href="#" id="{{id_filme}}" class="filme">{{nome}}</a></h2><p>Gênero: {{genero}}</p><p>Classificação: {{classificacao}} anos</p><p>{{descricao}}</p><br/><p class="horarios">Horário(s): </p><div class="h"></div></div>';
+		$result[] = $template;
+		$result = json_encode($result);
+		header("Content-type: text/javascript"); 
+		echo $_GET['jsoncallback'] . '(' . $result . ')'; 
+	}
+	
+	function maiorData(){
+		$datas = mysql_query("SELECT dia, mes, ano FROM relacional");
+		$data = array();
+		while($l = mysql_fetch_array($datas)){
+			$dia = $l['dia'];
+			$mes = $l['mes'];
+			$ano = $l['ano'];
+			$data[] = mktime(0, 0, 0, $mes, $dia, $ano);
+		}
+		return max($data);
+	}
+	
+	function datas() {
+		$maxData = maiorData();
+		$atual  = mktime(0, 0, 0, date("m")  , date("d"), date("Y"));
+		$datas = array();
+		$i = 1;
+		while ($atual <= $maxData) {
+			$data = date("Y-m-d", $atual);
+			$datas[] = $data;
+			$atual = mktime(0, 0, 0, date("m")  , date("d")+$i, date("Y"));
+			$i++;
+		}
+		return $datas;
+	}
+	
+	function filmesDisponiveis() {
+		$datas = datas();
+		$filmes = array();
+		foreach ($datas as $dia){
+			$val = explode("-", $dia);
+			$d = $val[2];
+			$m = $val[1];
+			$a = $val[0];
+			$filmes_sql = mysql_query("SELECT filmes.nome, filmes.id_filme FROM filmes JOIN relacional ON filmes.id_filme = relacional.id_filme WHERE relacional.dia = $d AND relacional.mes = $m AND relacional.ano = $a ORDER BY filmes.nome");
+			while($filme = mysql_fetch_array($filmes_sql)){
+				if(!in_array($filme, $filmes)){
+					$filmes[] = $filme;
+				}
+			}
+		}
+		$filmes = json_encode($filmes);
+		header("Content-type: text/javascript"); 
+		echo $_GET['jsoncallback'] . '(' . $filmes . ')';
+	}
+	
+	function datasDisponiveis() {
+		$datas = datas();
+		$datas = json_encode($datas);
+		header("Content-type: text/javascript"); 
+		echo $_GET['jsoncallback'] . '(' . $datas . ')';
+	}
+	
+	function locaisDisponiveis() {
+		$locais = array();
+		$locais_sql = mysql_query("SELECT id_localidade, nome FROM localidade ORDER BY nome");
+		while($local = mysql_fetch_array($locais_sql)){
+			$locais[] = $local;
+		}
+		$locais = json_encode($locais);
+		header("Content-type: text/javascript"); 
+		echo $_GET['jsoncallback'] . '(' . $locais . ')';
+	}
+	
+	function pesquisaFilme() {
+		$data = $_GET['data'];
+		$filme_id = $_GET['filme'];
+		$locais = array();
+		$localidades = array();
+		$fim = array();
+		$data  = explode("-", $data);
+		$ano = $data[0];
+		$mes = $data[1];
+		$dia = $data[2];
+		$filme_sql = mysql_query("SELECT * FROM filmes WHERE id_filme = $filme_id");
+		while($filme = mysql_fetch_array($filme_sql)){
+			$filmes[] = $filme;
+		}
+		$locais_sql = mysql_query("SELECT localidade.id_localidade, localidade.nome, relacional.horario FROM localidade JOIN relacional ON localidade.id_localidade = relacional.id_localidade WHERE relacional.id_filme = $filme_id AND relacional.dia = $dia AND relacional.mes = $mes AND relacional.ano = $ano");
+		while($local = mysql_fetch_array($locais_sql)){
+			$localidades[] = $local;
+		}
+		$fim[] = $localidades;
+		$fim[] = $filmes;
+		$fim = json_encode($fim);
+		header("Content-type: text/javascript"); 
+		echo $_GET['jsoncallback'] . '(' . $fim . ')';
+	}
+	
+	function pesquisaFilmeLocal() {
+		$data = $_GET['data'];
+		$filme_id = $_GET['filme'];
+		$local_id = $_GET['local'];
+		$locais = array();
+		$localidades = array();
+		$fim = array();
+		$data  = explode("-", $data);
+		$ano = $data[0];
+		$mes = $data[1];
+		$dia = $data[2];
+		$filme_sql = mysql_query("SELECT * FROM filmes WHERE id_filme = $filme_id");
+		while($filme = mysql_fetch_array($filme_sql)){
+			$filmes[] = $filme;
+		}
+		$locais_sql = mysql_query("SELECT localidade.id_localidade, localidade.nome, relacional.horario FROM localidade JOIN relacional ON localidade.id_localidade = relacional.id_localidade WHERE relacional.id_localidade = $local_id AND relacional.id_filme = $filme_id AND relacional.dia = $dia AND relacional.mes = $mes AND relacional.ano = $ano");
+		while($local = mysql_fetch_array($locais_sql)){
+			$localidades[] = $local;
+		}
+		$fim[] = $localidades;
+		$fim[] = $filmes;
+		$fim = json_encode($fim);
+		header("Content-type: text/javascript"); 
+		echo $_GET['jsoncallback'] . '(' . $fim . ')';
+	}
+	
+	function pesquisa() {
+		$data = $_GET['data'];
+		$data  = explode("-", $data);
+		$ano = $data[0];
+		$mes = $data[1];
+		$dia = $data[2];
+		$filmes_id = array();
+		$filmes = array();
+		$result = array();
+		$filmes_id_sql = mysql_query("SELECT DISTINCT filmes.id_filme FROM filmes JOIN relacional ON filmes.id_filme = relacional.id_filme WHERE relacional.dia = $dia AND relacional.mes = $mes AND relacional.ano = $ano ORDER BY filmes.nome");
+		while($filme_id = mysql_fetch_array($filmes_id_sql)){
+			$filmes_id[] = $filme_id;
+		}
+		foreach($filmes_id as $filme_id){
+			$localidades = array();
+			$filme_id = $filme_id[0];
+			$locais_sql = mysql_query("SELECT localidade.id_localidade, localidade.nome, relacional.horario FROM localidade JOIN relacional ON localidade.id_localidade = relacional.id_localidade WHERE relacional.id_filme = $filme_id AND relacional.dia = $dia AND relacional.mes = $mes AND relacional.ano = $ano");
+			while($local = mysql_fetch_array($locais_sql)){
+				$localidades[] = $local;
+			}
+			$filme_sql = mysql_query("SELECT * FROM filmes WHERE id_filme = $filme_id");
+			while($filme = mysql_fetch_array($filme_sql)){
+				$filme["horario"] = $localidades;
+				$filmes[] = $filme;
+			}
+		}
+		$filmes = json_encode($filmes);
+		header("Content-type: text/javascript"); 
+		echo $_GET['jsoncallback'] . '(' . $filmes . ')';	
+	}
+	
+	function pesquisaLocal() {
+		$data = $_GET['data'];
+		$local_id = $_GET['local'];
+		$data  = explode("-", $data);
+		$ano = $data[0];
+		$mes = $data[1];
+		$dia = $data[2];
+		$filmes_id = array();
+		$filmes = array();
+		$result = array();
+		$filmes_id_sql = mysql_query("SELECT DISTINCT filmes.id_filme FROM filmes JOIN relacional ON filmes.id_filme = relacional.id_filme WHERE relacional.id_localidade = $local_id AND relacional.dia = $dia AND relacional.mes = $mes AND relacional.ano = $ano ORDER BY filmes.nome");
+		while($filme_id = mysql_fetch_array($filmes_id_sql)){
+			$filmes_id[] = $filme_id;
+		}
+		foreach($filmes_id as $filme_id){
+			$localidades = array();
+			$filme_id = $filme_id[0];
+			$locais_sql = mysql_query("SELECT localidade.id_localidade, localidade.nome, relacional.horario FROM localidade JOIN relacional ON localidade.id_localidade = relacional.id_localidade WHERE relacional.id_filme = $filme_id AND relacional.id_localidade = $local_id AND relacional.dia = $dia AND relacional.mes = $mes AND relacional.ano = $ano");
+			while($local = mysql_fetch_array($locais_sql)){
+				$localidades[] = $local;
+			}
+			$filme_sql = mysql_query("SELECT * FROM filmes WHERE id_filme = $filme_id");
+			while($filme = mysql_fetch_array($filme_sql)){
+				$filme["horario"] = $localidades;
+				$filmes[] = $filme;
+			}
+		}
+		$filmes = json_encode($filmes);
+		header("Content-type: text/javascript"); 
+		echo $_GET['jsoncallback'] . '(' . $filmes . ')';
+	}
+	
+	mysql_close();
+
+?>
